564155cbe4e72c4d059e6b35553ba17816e76559
JohanUwUTzul <johanisraeltzulcoyoy@gmail.com>
2024-08-05 00:53:25 -0600

    primer poryecto integrador

---
+++ ProyectoIntegradoSpring1/ProyectoIntegradoSpring/src/main/java/ProyectoIntegradoSpring1/Ent1Application.java
@@ -0,0 +1,9 @@
+package ProyectoIntegradoSpring1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Ent1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Ent1Application.class, args);
+	}
+}
---
+++ ProyectoIntegradoSpring1/ProyectoIntegradoSpring/src/test/java/ProyectoIntegradoSpring1/Ent1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoIntegradoSpring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ent1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
